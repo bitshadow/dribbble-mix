9494ca3b3bff3bca7a2d08d6e75873f3279d7f81
Jignesh <jigneshhk1992@gmail.com>
2017-08-03 11:43:54 +0200

    use real urls

--- formatters/user.js
+++ formatters/user.js
@@ -17,29 +17,29 @@ module.exports = {
                     <div class="user">
                         <ul class="user_outer_ul">
                             <li class="user_li">
-                                <a href="${user.html_url}" class="user_link">
+                                <a href="${user.html_url}" class="user_link" target="_blank">
                                     <img class="user_thumbnail" src="${user.avatar_url}"></img>
                                 </a>
                             </li>
                             <li class="user_li">
                                 <div class="user_info">
-                                    <a class="user_name_link" href="${user.html_url}">${user.name}</a>
+                                    <a class="user_name_link" href="${user.html_url}" target="_blank">${user.name}</a>
                                 </div>
                                 <ul class="user_ul">
                                     <li class="user_li">
-                                        <a href="${user.followers_url}" class="user_link">
+                                        <a href="${user.html_url}/followers" class="user_link" target="_blank">
                                             <span class="user_label">Followers</span>
                                             <span class="user_count">${user.followers_count}</span>
                                         </a>
                                     </li>
                                     <li class="user_li">
-                                        <a href="${user.projects_url}" class="user_link">
+                                        <a href="${user.html_url}/projects" class="user_link" target="_blank">
                                             <span class="user_label">Projects</span>
                                             <span class="user_count">${user.projects_count}</span>
                                         </a>
                                     </li>
                                     <li class="user_li">
-                                        <a href="${user.likes_url}" class="user_link">
+                                        <a href="${user.html_url}/likes" class="user_link" target="_blank">
                                             <span class="user_label">Likes</span>
                                             <span class="user_count">${user.likes_received_count}</span>
                                         </a>
